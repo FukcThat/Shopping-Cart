dca04dd0bff1c5c77bed8f6784da15ff99addb51
FukcThat <63652e636f6465@gmail.com>
2025-03-29 22:37:50 +0100

    ADD: About page content

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <h3>I'm glad you're here!</h3>
+      <p>
+        But please don't try to buy anything. This page only exists for learning
+        purposes and none of the Items displayed on this site are for sale here,
+        because I don't own them.
+      </p>
+      <p>
+        Since you bothered to check this out, here's the cat tax for taking your
+        time:
+      </p>
+      {/* Put Gitty here */}
+    </div>
+  );
 }
