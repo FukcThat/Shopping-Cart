a76ec3247308d9366a00ee91147a57f339d59b8c
FukcThat <63652e636f6465@gmail.com>
2025-03-31 22:24:26 +0200

    STYLE: About page basic styling

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,19 +1,32 @@
 import React from "react";
+import "../styles/AboutPage.css"; // Make sure to create this!
 
 export default function AboutPage() {
   return (
-    <div>
-      <h3>I'm glad you're here!</h3>
-      <p>
-        But please don't try to buy anything. This page only exists for learning
-        purposes and none of the Items displayed on this site are for sale here,
-        because I don't own them.
-      </p>
-      <p>
-        Since you bothered to check this out, here's the cat tax for taking your
-        time:
-      </p>
-      {/* Put Gitty here */}
+    <div className="about-page">
+      <div className="about-content">
+        <h3 className="about-title">I'm glad you're here!</h3>
+
+        <p className="about-text">
+          But please don't try to buy anything. This page only exists for
+          learning purposes and none of the items displayed on this site are for
+          sale here, because I don't own them.
+        </p>
+
+        <p className="about-text">
+          Since you bothered to check this out, here's the cat tax for taking
+          your time:
+        </p>
+
+        <div className="cat-tax">
+          {/* Put Gitty here */}
+          <img
+            src="https://placekitten.com/400/300"
+            alt="Cat tax! You earned this."
+            className="cat-image"
+          />
+        </div>
+      </div>
     </div>
   );
 }
